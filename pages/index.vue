fd084057d0ac2e6dcde3e6428377bb4f0bcc04db
Ayat AL-ZAIDI <76471156+ayat0110@users.noreply.github.com>
2022-03-09 09:56:02 +0300

    İnitial commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <Hero-page />
+  <LargeCard-display v-for="cardInfo in largeCardInfo"
+  :key="cardInfo.id"
+  :cardsSection="cardInfo" />
+  </div>
+</template>
+
+<script>
+import {largeCardSections} from "~/assets/data.js"
+export default {
+ data(){
+   return{
+     largeCardInfo: largeCardSections
+   }
+ }
+}
+</script>
